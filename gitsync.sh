56b8ed117dfd92294742d624e0b7b1b936a6dc82
infuzz <backend@klicat.com>
2019-08-06 17:29:48 +0200

    Commit by infuzz

--- gitsync.sh
+++ gitsync.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 git -c user.email=backend@klicat.com -c user.name=infuzz pull origin master
 git -c user.email=backend@klicat.com -c user.name=infuzz add .
 git -c user.email=backend@klicat.com -c user.name=infuzz commit -a -m "Commit by infuzz"
